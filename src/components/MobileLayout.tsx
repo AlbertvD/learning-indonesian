3ae765f101d40cfd1785fe6c944195a99f8baf28
Albert van Duijn <albert@macbook-air.duin.home>
2026-03-28 08:33:47 +0100

    feat: add home icon to mobile bottom nav

--- src/components/MobileLayout.tsx
+++ src/components/MobileLayout.tsx
@@ -1,11 +1,12 @@
 // src/components/MobileLayout.tsx
 import { Outlet, NavLink } from 'react-router-dom'
 import { useMantineColorScheme } from '@mantine/core'
-import { IconBook, IconHeadphones, IconCards, IconTrophy, IconUser } from '@tabler/icons-react'
+import { IconBook, IconHeadphones, IconCards, IconTrophy, IconUser, IconHome } from '@tabler/icons-react'
 import classes from './MobileLayout.module.css'
 
 const navItems = [
   { icon: <IconUser size={22} />,       path: '/profile' },
+  { icon: <IconHome size={22} />,       path: '/' },
   { icon: <IconBook size={22} />,        path: '/lessons' },
   { icon: <IconHeadphones size={22} />, path: '/podcasts' },
   { icon: <IconCards size={22} />,      path: '/sets' },
